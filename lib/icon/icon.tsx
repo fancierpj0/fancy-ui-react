db7c7956bc7fe2a1ad6cff56fb2c2195f6487f88
Chris <757541811@qq.com>
2019-07-06 20:43:12 +0800

    '创建icon目录'

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import './importIcons';
 import './icon.scss';
-import classes from './helpers/classes';
+import classes from '../helpers/classes';
 
 //svg能接受的属性都可以接受，并且事件的target为SVGElement
 interface IconProps extends React.SVGAttributes<SVGElement> {
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // @ts-ignore
 import ReactDOM from "react-dom";
-import Icon from './icon';
+import Icon from './icon/icon';
 
 //这里e不用在写type了，可以根据函数的type推测出
 const fn: React.MouseEventHandler = (e) => {
