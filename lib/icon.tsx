dd2fcd2004ffe92c42e782d71e2cccc7f9f1047a
Chris <757541811@qq.com>
2019-07-06 20:06:31 +0800

    '让icon支持传递更多的classname'

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,17 +1,22 @@
 // @ts-ignore
 import React from 'react';
-import './importIcons'
-import './icon.scss'
+import './importIcons';
+import './icon.scss';
+import classes from './helpers/classes';
 
-interface IconProps {
+//svg能接受的属性都可以接受，并且事件的target为SVGElement
+interface IconProps extends React.SVGAttributes<SVGElement>{
     name: string;
-    onClick: React.MouseEventHandler<SVGElement|SVGUseElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
+    const {className,...restProps} = props;
+
     return (
         //svg就是就一个内联块元素(inline-block)
-        <svg className="fui-icon" onClick={props.onClick}>
+        <svg className={classes('fui-icon',className)}
+             {...restProps}
+        >
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -10,5 +10,9 @@ const fn: React.MouseEventHandler = (e) => {
 };
 
 ReactDOM.render(<div>
-    <Icon name="wechat" onClick={fn}/>
+    <Icon name="wechat"
+          onClick={fn}
+          onMouseEnter={()=>console.log('enter')}
+          onMouseLeave={()=>console.log('leave')}
+    />
 </div>, document.querySelector('#root'));
