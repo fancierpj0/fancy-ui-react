9c914a5b726a56904223b1ae081852d1d37852bc
Chris <757541811@qq.com>
2019-07-07 18:54:24 +0800

    添加job:publish和设置npm的token

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,18 +1,1 @@
-// @ts-ignore
-import React from 'react';
-// @ts-ignore
-import ReactDOM from "react-dom";
-import Icon from './icon/icon';
-
-//这里e不用在写type了，可以根据函数的type推测出
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e);
-};
-
-ReactDOM.render(<div>
-    <Icon name="wechat"
-          onClick={fn}
-          onMouseEnter={()=>console.log('enter')}
-          onMouseLeave={()=>console.log('leave')}
-    />
-</div>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon'
