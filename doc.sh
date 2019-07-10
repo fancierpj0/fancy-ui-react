603388773ac9ba98a98ddb08964936cb49e03867
Chris <757541811@qq.com>
2019-07-11 01:15:10 +0800

    'init'

--- doc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-npm run doc
-git checkout gh-pages
-mv -f doc/* ./
-git add -A
-git commit -m "update"
-git push
-git checkout -
--- jest.config.ci.js
+++
@@ -1,9 +0,0 @@
-const base = require('./jest.config');
-module.exports = Object.assign({}, base, {
-    reporters:["jest-junit"]  //jest-junit主要给ci用得 在package.json的脚本中进行相关配置
-    ,collectCoverage:true
-    //默认__tests__就不会纳入覆盖率测试(覆盖率测试当然要针对于非test文件)
-    ,collectCoverageFrom:["lib/**/*.{ts,tsx}","!**/node_modules/**"]
-    ,coverageDirectory:'coverage' //生成的报告放到哪里
-    ,coverageReporters:["text","lcov"] //要哪些报告 text是控制台的输出 lcov是一种有名的报告形式
-});
