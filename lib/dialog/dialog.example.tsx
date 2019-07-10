d7115466c16c16f5a5c63b40871e7d14a9c39c90
Chris <757541811@qq.com>
2019-07-10 11:22:57 +0800

    'dialog功能实现'

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
-import Dialog from './dialog';
+import Dialog, {alert, confirm, modal} from './dialog';
 
 export default function () {
   const [x, setX] = useState(false);
   const [y, setY] = useState(false);
+  const openModal = () => {
+    //函数返回操作内部变量的api
+    const close = modal(<h1>你好
+      <button onClick={() => close()}>close</button>
+    </h1>);
+  };
 
   return (
     <>
-      <div style={{position:'relative',zIndex:10,border:'1px solid red',color:'red'}}> {/* TODO 点第二个example的button的时候，这个div会浮起来 */}
+      <div style={{
+        position: 'relative',
+        zIndex: 10,
+        border: '1px solid red',
+        color: 'red'
+      }}> {/* TODO 点第二个example的button的时候，这个div会浮起来 */}
         <h1>example 1</h1>
         <button onClick={() => setX(!x)}>click</button>
         <Dialog visible={x} buttons={[
@@ -20,7 +31,12 @@ export default function () {
         </Dialog>
       </div>
 
-      <div style={{position:'relative',zIndex:9,border:'1px solid blue',color:'blue'}}> {/* TODO 并且这个Dialog的父比上面那个的z-index还低，那么不管怎么提高Dialog的层级也没用 */}
+      <div style={{
+        position: 'relative',
+        zIndex: 9,
+        border: '1px solid blue',
+        color: 'blue'
+      }}> {/* TODO 并且这个Dialog的父比上面那个的z-index还低，那么不管怎么提高Dialog的层级也没用 */}
         <h1>example 2</h1>
         <button onClick={() => setY(!y)}>click</button>
         <Dialog visible={y} buttons={[
@@ -32,6 +48,23 @@ export default function () {
         >
           <strong>hi</strong>
         </Dialog>
+      </div>
+
+      <div>
+        <h1>example 3</h1>
+        <button onClick={() => alert('1')}>alert</button>
+        <button onClick={() => confirm('1', () => {
+          console.log('yes')
+        }, () => {
+          console.log('no')
+        })}>confirm
+        </button>
+      </div>
+
+
+      <div>
+        <h1>example 4</h1>
+        <button onClick={openModal}>modal</button>
       </div>
     </>
   )
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,21 +1,21 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, ReactFragment, ReactNode} from 'react';
 import ReactDOM from 'react-dom';
 import './dialog.scss';
 import Icon from "../icon/icon";
 import {scopedClassMaker} from "../helpers/classes";
 
 interface Props {
-  visible:boolean;
-  buttons:Array<ReactElement>;
-  onClose:React.MouseEventHandler;
-  closeOnClickMask?:boolean;
+  visible: boolean;
+  buttons?: Array<ReactElement>;
+  onClose: React.MouseEventHandler;
+  closeOnClickMask?: boolean;
 }
 
 const scopedClass = scopedClassMaker('fui-dialog');
 const sc = scopedClass;
 
-const Dialog:React.FunctionComponent<Props> = (props) => {
-  const onClickClose:React.MouseEventHandler = (e) => {
+const Dialog: React.FunctionComponent<Props> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
     props.onClose(e)
   };
 
@@ -39,19 +39,89 @@ const Dialog:React.FunctionComponent<Props> = (props) => {
         <main className={sc('main')}>
           {props.children}
         </main>
-        <footer className={sc('footer')}>
-          {/* 直接给key是不允许的，需要clone */}
-          {props.buttons.map((button,index)=>React.cloneElement(button,{key:index}))}
-        </footer>
+        {
+          props.buttons && props.buttons.length>0 &&
+          <footer className={sc('footer')}>
+            {/* 直接给key是不允许的，需要clone */}
+            {props.buttons && props.buttons.map((button, index) => React.cloneElement(button, {key: index}))}
+          </footer>
+        }
       </div>
     </>
-    : null
+    : null;
 
-  return ReactDOM.createPortal(x,document.body);
+  return ReactDOM.createPortal(x, document.body);
 };
 
 Dialog.defaultProps = {
   closeOnClickMask: false
 };
+
+const confirm = (content: string, yes?: () => void, no?: () => void) => {
+  const onYes = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    yes && yes();
+  };
+  const onNo = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    no && no();
+  };
+  const component = (
+    <Dialog
+      visible={true} onClose={onNo}
+      buttons={[
+        <button onClick={onYes}>yes</button>
+        , <button onClick={onNo}>no</button>]}
+    >
+      {content}
+    </Dialog>);
+  const div = document.createElement('div');
+  document.body.append(div);
+  ReactDOM.render(component, div);
+};
+
+const alert = (content: string) => {
+  const onClose = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);//事件销毁 垃圾回收的一些工作
+    div.remove();
+  };
+  const component = <Dialog
+    visible={true}
+    onClose={onClose}
+    buttons={[<button onClick={onClose}>ok</button>]}
+  >{content}</Dialog>;
+  const div = document.createElement('div');
+  document.body.append(div);
+  ReactDOM.render(component, div);
+};
+
+
+const modal = (content: ReactNode | ReactFragment) => {
+  const onClose = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);//事件销毁 垃圾回收的一些工作
+    div.remove();
+  };
+  const component = (
+    <Dialog
+      onClose={onClose}
+      visible={true}>
+      {content}
+    </Dialog>
+  );
+
+  const div = document.createElement('div');
+  document.body.append(div);
+  ReactDOM.render(component, div);
+
+  return onClose;
+};
+
+export {alert, confirm, modal};
 
 export default Dialog;
