554b3db0ad0f23a5aa23d12845bcb5cefc384c76
Chris <757541811@qq.com>
2019-07-10 23:21:55 +0800

    '优化demo展示'

--- demo.tsx
+++ demo.tsx
@@ -1,16 +1,36 @@
 import * as React from 'react';
+import Highlight, {defaultProps} from 'prism-react-renderer';
+import {useState} from "react";
 
 interface Props {
   code:string;
 }
 
 const Demo:React.FunctionComponent<Props>=(props) => {
+  const [codeVisible,setCodeVisible] = useState(false);
   return (
     <div>
-      {props.children}
-      <pre>
-        {props.code}
-      </pre>
+      <div className="demo">
+        {props.children}
+      </div>
+      <div>
+        <button onClick={()=>setCodeVisible(!codeVisible)}>查看代码</button>
+        {codeVisible&&<Highlight {...defaultProps} code={props.code} language="jsx">
+          {
+            ({className,style,tokens,getLineProps,getTokenProps})=>(
+              <pre className={className} style={style}>
+              {tokens.map((line,i)=>(
+                <div {...getLineProps({line,key:i})}>
+                  {line.map((token,key)=>(
+                    <span {...getTokenProps({token,key})}/>
+                  ))}
+                </div>
+              ))}
+            </pre>
+            )
+          }
+        </Highlight>}
+      </div>
     </div>
   )
 };
